668a536b9490d780285ff800d10c57dc6182a529
malick-tammal <malick.tammal15@gmail.com>
2026-02-18 10:48:11 +0100

    Init

---
+++ scripts/notifications.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+notifications=$(swaync-client -c)
+if [ "$notifications" -gt 0 ]; then
+    echo "{\"text\": \"   $notifications\", \"class\": \"notif\"}"
+else
+    echo "{\"text\": \"\", \"class\": \"empty\"}"
+fi
